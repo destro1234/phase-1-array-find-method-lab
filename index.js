9a8aeb8e1e2c996e08a03cd5d5009a36f67bc58a
Elias <eliasventura23@gmail.com>
2022-03-23 13:11:52 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function findWin(obj) {
+    return obj.result === "W";
+}
+
+
+function superbowlWin (record) {
+    let win = record.find(findWin)
+    if (win) {
+        return win.year;
+    }
+    else 
+        return undefined;
+}
